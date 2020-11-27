b3fc6135ebbbbfca54d9d68f46a9426fa728c0e6
Vovan1608 <vovan1608@gmail.com>
2020-11-27 18:11:29 +0200

    added datastore fail, added method add

---
+++ scripts/datastore.js
@@ -0,0 +1,21 @@
+"use strict";
+
+(function (window) {
+	/* 
+	Если у объекта window уже есть свойство App, мы присвоим его локальной переменной App. 
+	Если нет, «ярлык» App будет ссылаться на новый пустой объект, представленный {}.
+	*/
+	let App = window.App || {};
+	
+	function DataStore() {
+		this.data = {};
+	}
+	// Экземпляр DataStore будет сохранять информацию о заказе (val), используя адрес
+	// электронной почты посетителя (key).
+	DataStore.prototype.add = function (key, val) {
+		this.data[key] = val;
+	};
+	// в объекте App создаем св-во DataStore, куда записываем функцию
+	App.DataStore = DataStore;
+	window.App = App;
+})(window);
