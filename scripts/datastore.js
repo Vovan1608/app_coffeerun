69ca2fb62ab1654cd3791e939705ade67f39666a
Vovan1608 <vovan1608@gmail.com>
2020-11-28 10:11:19 +0200

    added method remove()

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -10,18 +10,29 @@
 	function DataStore() {
 		this.data = {};
 	}
+
 	// Экземпляр DataStore будет сохранять информацию о заказе (val), используя адрес
 	// электронной почты посетителя (key).
 	DataStore.prototype.add = function (key, val) {
 		this.data[key] = val;
 	};
+
+	// метод get, который принимает на входе ключ, выполняет поиск значения
+	// для него в свойстве data экземпляра и возвращает это значение
 	DataStore.prototype.get = function (key) {
 		return this.data[key];
 	};
-		DataStore.prototype.getAll = function () {
+
+	// возвращает ссылку на свойство data
+	DataStore.prototype.getAll = function () {
 		return this.data;
 	};
-	// в объекте App создаем св-во DataStore, куда записываем функцию
+
+	// метод для удаления информации
+	DataStore.prototype.remove = function (key) {
+		delete this.data[key];
+	};
+	// в объекте App создаем св-во DataStore, куда записываем конструктор
 	App.DataStore = DataStore;
 	window.App = App;
 })(window);
