ea1d1040398fe676d77443bc5d961738c864aacf
Vovan1608 <vovan1608@gmail.com>
2020-11-28 10:26:12 +0200

    added method createOrder()

--- scripts/truck.js
+++ scripts/truck.js
@@ -2,9 +2,18 @@
 
 (function (window) {
 	let App = window.App || {};
-	function Truck() {
-	}
 	
+	function Truck(truckId, db) {
+		this.truckId = truckId;
+		this.db = db;
+	}
+
+	// вызывать метод add объекта DataStore, чтобы сохранить заказ кофе
+	Truck.prototype.createOrder = function (order) {
+		console.log('Adding order for ' + order.emailAddress);
+		this.db.add(order.emailAddress, order);
+	};
+
 	App.Truck = Truck;
 	window.App = App;
-	})(window);
+})(window);
