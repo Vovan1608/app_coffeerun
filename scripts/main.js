9c88f778fa1cbf3d62e7733f29d6ff5d698c8457
Vovan1608 <vovan1608@gmail.com>
2020-11-28 14:17:08 +0200

    renamed to My Truck in main.js str 7

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@
 	let App = window.App;
 	let Truck = App.Truck;
 	let DataStore = App.DataStore;
-	let myTruck = new Truck('ncc-1701', new DataStore());
+	let myTruck = new Truck('My Truck', new DataStore());
 	// экспортируем myTruck в глобальное пространство имен:
 	window.myTruck = myTruck;
 })(window);
